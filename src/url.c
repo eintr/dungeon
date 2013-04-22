79bee76e87d6e45b6322df2078f9fb4168f4ccf8
John Bull <haiqing1@staff.sina.com.cn>
2013-04-22 15:09:46 +0800

    Dev: URL coded. Not complete but sufficent.

---
+++ src/url.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "url.h"
+
+int url_brokedown(struct url_brokedown_st *result, const char *str)
+{
+	uint8_t *pos, *ptr, *port_asciiz, *url=(void*)str;
+	memvec_t tmp, host, port, path, param;
+
+	pos = strchr(url, ':');
+	if (pos!=NULL) {
+		result->protocol.ptr = url;
+		result->protocol.size = url-pos;
+
+		host.ptr = pos+3;
+		pos = strchr(host.ptr, '/');
+		host.size = pos - host.ptr;
+		for (ptr=host.ptr;ptr<pos;ptr++) {
+			if (*ptr==':') {
+				break;
+			}
+		}
+		if (ptr==pos) {	// : not found, port is 80
+			result->host.ptr = host.ptr;
+			result->host.size = host.size;
+			result->port = 0;
+		} else { // : found, extract host:port
+			host.size = ptr-host.ptr;
+			result->host.ptr = host.ptr;
+			result->host.size = host.size;
+
+			port.ptr = ptr+1;
+			port.size = pos-ptr;
+
+			port_asciiz = string_toc(&port);
+			result->port = atoi(port_asciiz);
+			free(port_asciiz);
+		}
+	} else { // There is NO 'http://' part
+		result->protocol.ptr = NULL;
+		result->protocol.size = 0;
+		result->host.ptr = NULL;
+		result->host.size = 0;
+		result->port = 0;
+		pos = url;
+	}
+	// pos points to the beginning of path here.
+	tmp.ptr = pos;
+	pos = strchr(tmp.ptr, '?');
+	if (pos==NULL) {	// No parameters found
+		result->path.ptr = tmp.ptr;
+		result->path.size = strlen(tmp.ptr);
+		result->param.ptr = NULL;
+		result->param.size = 0;
+	} else {
+		result->path.ptr = tmp.ptr;
+		result->path.size = pos - tmp.ptr;
+		result->param.ptr = pos;
+		result->param.size = strlen(pos);
+	}
+
+	return 0;
+}
+
+
--- src/url.h
+++ src/url.h
@@ -7,17 +7,12 @@
  * A broken down url.
  */
 struct url_brokedown_st {
-	string_t *protocol;		// Such as: "http", "https", "ftp", ...
-	string_t *host;			// Such as: "www.abc.com"
+	string_t protocol;		// Such as: "http", "https", "ftp", ...
+	string_t host;			// Such as: "www.abc.com"
 	uint16_t port;			// Port number, in host byteorder
-	string_t *api;			// Such as: "/aaa/bbb/ccc/asd.php"
-	string_t *param;		// Such as: "?param1=123&param2=casda"
+	string_t path;			// Such as: "/aaa/bbb/ccc/asd.php"
+	string_t param;			// Such as: "?param1=123&param2=casda"
 };
-
-/*
- * Create an url struct
- */
-struct url_brokedown_st *url_new(void);
 
 /*
  * Break down a header string into an url struct
@@ -28,11 +23,6 @@ int url_brokedown(struct url_brokedown_st *, const char *url);
  * Modify a broke down header, remove some parts which needed only by the proxy
  */
 int url_deproxy(struct url_brokedown_st*);
-
-/*
- * Destroy an url struct
- */
-int url_delete(struct url_st *);
 
 /*
  * Translate an url struct into a c string

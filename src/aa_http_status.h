a6f770fadde415a80fa4545918540fea7bd0f999
John Bull <haiqing1@staff.sina.com.cn>
2013-06-28 10:46:46 +0800

    Fix: Use configure info.

--- src/aa_http_status.h
+++ src/aa_http_status.h
@@ -1,8 +1,10 @@
 #ifndef HTTP_STATUS_CODE_H
 #define HTTP_STATUS_CODE_H
 
-#define HTTP_503 "HTTP/1.1 503 Service Temporarily Unavailable\r\nServer: aa_proxy\r\nContent-Length: 0\r\n\r\n"
-#define HTTP_504 "HTTP/1.1 504 Gateway Timeout\r\nServer: aa_proxy\r\nContent-Length: 0\r\n\r\n"
+#include "aa_conf.h"
+
+#define HTTP_503 "HTTP/1.1 503 Service Temporarily Unavailable\r\nServer: " APPNAME "\r\nContent-Length: 0\r\n\r\n"
+#define HTTP_504 "HTTP/1.1 504 Gateway Timeout\r\nServer: " APPNAME "\r\nContent-Length: 0\r\n\r\n"
 
 
 #endif

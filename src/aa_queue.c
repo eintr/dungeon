1032208322cd3f9097e4915c6ef0532cc135021c
John Bull <haiqing1@staff.sina.com.cn>
2013-07-11 16:30:14 +0800

    Fix: Use header file correctly.

--- src/aa_queue.c
+++ src/aa_queue.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "aa_queue.h"
 
 queue *queue_init(uint32_t max)
--- src/aa_queue.h
+++ src/aa_queue.h
@@ -1,8 +1,6 @@
 #ifndef _AA_QUEUE_H
 #define _AA_QUEUE_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 
 #define CAS(ptr, oldv, newv) __sync_bool_compare_and_swap(ptr, oldv, newv)

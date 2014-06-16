5ab3f08a62dd871901943c9da8b6ec1752764b2f
牛海青 <haiqing1@staff.sina.com.cn>
2014-06-16 16:02:00 +0800

    Deal with type cross reference.

--- src/imp_soul.h
+++ src/imp_soul.h
@@ -12,13 +12,13 @@ enum enum_driver_retcode {
 };
 
 struct imp_st;
-typedef struct imp_st imp_t;
+//typedef struct imp_st imp_t;
 
 typedef struct imp_soul_st {
-	void *(*fsm_new)(imp_t*);						/**< Imp init. Called by imp_summon(), the pointer passed in must point to a completed imp_t(ptr->memory must has been allocated) , this call back could do some init operations about imp_t.  */
-	int (*fsm_delete)(imp_t*);						/**< Imp delete */
-	enum enum_driver_retcode (*fsm_driver)(imp_t *);	/**< Core action of imp */
-	void *(*fsm_serialize)(imp_t*);					/**< Report imp soul status */
+	void *(*fsm_new)(struct imp_st*);						/**< Imp init. Called by imp_summon(), the pointer passed in must point to a completed imp_t(ptr->memory must has been allocated) , this call back could do some init operations about imp_t.  */
+	int (*fsm_delete)(struct imp_st*);						/**< Imp delete. \param Should be imp_t* */
+	enum enum_driver_retcode (*fsm_driver)(struct imp_st*);	/**< Core action of imp. \param Should be imp_t* */
+	void *(*fsm_serialize)(struct imp_st*);					/**< Report imp soul status. \param Should be imp_t* */
 } imp_soul_t;
 
 #endif

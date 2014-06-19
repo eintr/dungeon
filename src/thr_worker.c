ff6a27175bf629a7d6e80d7c42b415a3ef52d7d6
牛海青 <haiqing1@staff.sina.com.cn>
2014-06-19 16:17:09 +0800

    Adjusted scheduler algorithm for more balance while different loads mixed.

--- src/thr_worker.c
+++ src/thr_worker.c
@@ -27,7 +27,9 @@ static void *thr_worker(void *p)
 	int err, num;
 	struct epoll_event ioev[IOEV_SIZE];
 	sigset_t allsig;
-	int i;
+	int i, r, rweight;
+	struct timeval t0, t1, dt;
+	int epoll_timeout_ms, dt_ms;
 
 	sigfillset(&allsig);
 	pthread_sigmask(SIG_BLOCK, &allsig, NULL);
@@ -36,9 +38,14 @@ static void *thr_worker(void *p)
 
 	worker_id = (int)p;
 
+	rweight=100;
+	epoll_timeout_ms = 10;
+
 	while (!worker_quit) {
+		//fprintf(stderr, "rweight=%d,\tepoll_timeout_ms=%d            \r", rweight, epoll_timeout_ms);
 		/* deal node in run queue */
-		while (1) {
+		gettimeofday(&t0, NULL);
+		for (r=0; r<rweight; ++r) {
 			err = queue_dequeue_nb(dungeon_heart->run_queue, &node);
 			if (err < 0) {
 				//mylog(L_DEBUG, "run_queue is empty.");
@@ -46,12 +53,38 @@ static void *thr_worker(void *p)
 			}
 			imp_driver(node);
 		}
+		gettimeofday(&t1, NULL);
 
-		num = epoll_wait(dungeon_heart->epoll_fd, ioev, IOEV_SIZE, 1000);
+		/** If runqueue is too busy, reduce rweight and epoll_timeout to avoid connection block */
+		timersub(&t1, &t0, &dt);
+		dt_ms = dt.tv_sec*1000+dt.tv_usec/1000;
+		if (dt_ms > 100) {
+			rweight = (int)((double)rweight*100.0F/(double)dt_ms);
+			epoll_timeout_ms = (int)(epoll_timeout_ms*0.618F);
+		}
+
+		/** If runqueue is too idle, increase block timeout to supress CPU load */
+		if (r==0) {
+			epoll_timeout_ms = (int)(epoll_timeout_ms*1.618F);
+			if (epoll_timeout_ms>1000) {
+				epoll_timeout_ms=1000;	/** But never block longer than 1s */
+			}
+			rweight = (int)(rweight*1.618F);
+			if (rweight>100000) {
+				rweight=100000;
+			}
+		}
+
+		num = epoll_wait(dungeon_heart->epoll_fd, ioev, IOEV_SIZE, epoll_timeout_ms);
 		if (num<0) {
 			mylog(L_ERR, "epoll_wait(): %m");
 		} else if (num==0) {
-			mylog(L_DEBUG, "epoll_wait() timed out.");
+			/** If IO is too idle, increase block timeout to supress CPU load */
+			epoll_timeout_ms = (int)(epoll_timeout_ms*1.618F);
+			if (epoll_timeout_ms>1000) {
+				epoll_timeout_ms=1000;	/** But never block longer than 1s */
+			}
+			//mylog(L_DEBUG, "epoll_wait() timed out.");
 		} else {
 			//mylog(L_DEBUG, "Worker[%d]: epoll_wait got %d/1000 events", worker_id, num);
 			for (i=0;i<num;++i) {

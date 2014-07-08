5f2eae1e236d559eff711f1bb789ede7e0d0b836
牛海青 <haiqing1@staff.sina.com.cn>
2014-07-08 12:12:13 +0800

    Added offsetof()

--- src/util_misc.h
+++ src/util_misc.h
@@ -1,7 +1,14 @@
 #ifndef UTIL_MISC_H
 #define UTIL_MISC_H
 
-#define	offsetof(T, M)	((int)(((T*)0)->M))
+#include <dungeon.h>
+
+#define	offsetof(T, M)	((intptr_t)(((T*)0)->M))
+
+static inline int delta_t(void)
+{
+    return time(NULL)-dungeon_heart->create_time.tv_sec;
+}
 
 #endif
 

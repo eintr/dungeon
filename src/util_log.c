5fdf69b1f741307b9ce59a8ec68ed252e37963a0
牛海青 <haiqing1@staff.sina.com.cn>
2015-01-28 18:12:29 +0800

    Added missing header.

--- src/util_log.c
+++ src/util_log.c
@@ -1,9 +1,10 @@
+/** \cond 0 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdarg.h>
 #include <errno.h>
 #include <pthread.h>
+/** \endcond */
 
 #define SYSLOG_NAMES 1
 #include "util_log.h"

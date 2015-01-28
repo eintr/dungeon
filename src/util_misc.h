058f7f6cf49de32f3487b8445ac3451541e0807b
牛海青 <haiqing1@staff.sina.com.cn>
2015-01-28 18:12:08 +0800

    Added systimestamp_ms()

--- src/util_misc.h
+++ src/util_misc.h
@@ -1,14 +1,28 @@
 #ifndef UTIL_MISC_H
 #define UTIL_MISC_H
 
-#include <dungeon.h>
+/** \cond 0 */
 #include <alloca.h>
+#include <sys/time.h>
+#include <string.h>
+/** \endcond */
+
+#include <dungeon.h>
 
 #define	offsetof(T, M)	((intptr_t)(((T*)0)->M))
 
 static inline int delta_t(void)
 {
     return time(NULL)-dungeon_heart->create_time.tv_sec;
+}
+
+static inline time_t systimestamp_ms(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
 static inline cJSON *cpuset_to_cjson(cpu_set_t *cpuset, int max)
